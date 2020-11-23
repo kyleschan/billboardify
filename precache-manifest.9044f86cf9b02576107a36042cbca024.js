a581316b3540af77889a5fadb3caeee554660a06
Kyle Chan <kyleschan@yahoo.com>
2020-11-22 21:22:31 -0800

    Updates

---
+++ precache-manifest.9044f86cf9b02576107a36042cbca024.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcb5fb1a0974c5d20fb1",
+    "url": "/billboardify/static/css/main.a8e2c948.chunk.css"
+  },
+  {
+    "revision": "fcb5fb1a0974c5d20fb1",
+    "url": "/billboardify/static/js/main.a5348709.chunk.js"
+  },
+  {
+    "revision": "12b15fe4403acabd28e3",
+    "url": "/billboardify/static/js/runtime~main.73cf3935.js"
+  },
+  {
+    "revision": "0e5db2f2e2fc15767d88",
+    "url": "/billboardify/static/css/2.447216ae.chunk.css"
+  },
+  {
+    "revision": "0e5db2f2e2fc15767d88",
+    "url": "/billboardify/static/js/2.c355c915.chunk.js"
+  },
+  {
+    "revision": "ebc6b10da75a75ad43ef42c83a6306b0",
+    "url": "/billboardify/static/media/top-tracks.ebc6b10d.jpg"
+  },
+  {
+    "revision": "cdedb10be883d087d74be330b83a7342",
+    "url": "/billboardify/static/media/bb-placeholder.cdedb10b.jpg"
+  },
+  {
+    "revision": "e0c8670a4bedbea4e9e17d867d531ee4",
+    "url": "/billboardify/static/media/discover.e0c8670a.jpg"
+  },
+  {
+    "revision": "f63e7535b21682a4a497ad7075f6d76d",
+    "url": "/billboardify/index.html"
+  }
+];
