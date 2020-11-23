e6a91e1aeb6d838598ffd98b7a3fde6859563b11
Kyle Chan <kyleschan@yahoo.com>
2020-11-23 14:42:06 -0800

    put realm id in env.js

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ import ENV from '../env';
 
 const config = {
   API_URL: 'https://api.spotify.com/v1',
-  REALM_APP_ID: 'playlist-app-ectuj',
+  REALM_APP_ID: ENV.REALM_APP_ID,
   SPOTIFY_AUTHORIZE_URL: 'https://accounts.spotify.com/authorize',
   SPOTIFY_AUTH_SCOPES: 'playlist-modify-public',
   SPOTIFY_CLIENT_ID: ENV.SPOTIFY_CLIENT_ID,
