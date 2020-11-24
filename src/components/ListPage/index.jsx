52e02fb1fd5249b5f353724d7ab8f953482b9e5d
Kyle Chan <kyleschan@yahoo.com>
2020-11-23 23:43:44 -0800

    changed display dotw to Sat.

--- src/components/ListPage/index.jsx
+++ src/components/ListPage/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import changeThemeColor from '../../services/change-theme';
+import {toSaturday} from '../../concepts/calendar';
 import './ListPage.css';
 
 class ListPage extends Component {
@@ -18,7 +19,7 @@ class ListPage extends Component {
       <div className="list-page">
         <div style={{ backgroundImage: `url(${headerImageSrc})` }} className="list-page__image" />
         <h1 className="list-page__title">Billboard Top 50</h1>
-        <h2 className="list-page__subtitle">Week of {title}</h2>
+        <h2 className="list-page__subtitle">Week of {toSaturday(title)}</h2>
         <div className="list-page__content">
           <div className="list-page__list">{children}</div>
         </div>
--- src/concepts/calendar.js
+++ src/concepts/calendar.js
@@ -2,6 +2,7 @@ import moment from "moment";
 
 const DATE_STRING_END = 10;
 const WEDNESDAY_ISO_INT = 3;
+const SATURDAY_ISO_INT = 6;
 
 export const addDays = (date, days) => {
   var newDate = new Date(date);
@@ -13,6 +14,11 @@ export const adjustDate = date => {
   var newDate = addDays(date, WEDNESDAY_ISO_INT - date.getDay());
   const today = new Date(moment().toDate());
   return newDate <= today ? newDate : addDays(newDate, -7);
+}
+
+export const toSaturday = dateStr => {
+  const oldDate = new Date(dateStr);
+  return dateString(addDays(oldDate, SATURDAY_ISO_INT - oldDate.getDay()));
 }
 
 export const dateString = date => moment(date).format().substring(0, DATE_STRING_END);
